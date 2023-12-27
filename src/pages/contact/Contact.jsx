1894cbdbf3c65727386a152d655020f9dd322f08
Mickyj77 <emrex1291@gmail.com>
2023-12-27 11:49:47 +0100

    fixed error

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,44 +2,46 @@ import "./Contact.css";
 // import { motion } from "framer-motion";
 
 export const Contact = () => {
-	return (
-		<>
-			<section id="contact" className="contactSection">
-				<div className="contactContainer">
-					<div className="contactHeader">
-						<p>CONTACT</p>
-					</div>
+  return (
+    <>
+      <section id="contact" className="contactSection">
+        <div className="contactContainer">
+          <div className="contactHeader">
+            <p>CONTACT</p>
+          </div>
 
-					<div className="contactInfo">
-						<p>
-							Ready to bring your vision to life or just want to chat? Reach
-							out, and {"let's"} create something memorable together.
-						</p>
-						<p>
-							{"I'm"} here to listen, collaborate, and craft web app solutions
-							that resonate.
-						</p>
-					</div>
+          <div className="contactInfo">
+            <p>
+              Ready to bring your vision to life or just want to chat? Reach
+              out, and {"let's"} create something memorable together.
+            </p>
+            <p>
+              {"I'm"} here to listen, collaborate, and craft web app solutions
+              that resonate.
+            </p>
+          </div>
 
-					<div className="contactMail">
-						<a href="mailto:emrex1291@gmail.com">emrex1291@gmail.com</a>
-					</div>
+          <div className="contactMail">
+            <a href="mailto:emrex1291@gmail.com">emrex1291@gmail.com</a>
+          </div>
 
-					<div className="contactFooter">
-						<div className="contactName">
-							<p>Mike Dev©</p>
-						</div>
+          <div className="contactFooter">
+            <div className="contactName">
+              <p>Mike Dev©</p>
+            </div>
 
-						<div className="contactLink">
-							<a href="https://github.com/Mickyj70">GITHUB</a>
-							<a href="https://x.com/IAmMicky7?t=jc1z6hLxPSZk5wnRwFxa5w&s=09">
-								TWITTER
-							</a>
-							<a href="www.linkedin.com/in/excel-emma-457b61201">LINKEDIN</a>
-						</div>
-					</div>
-				</div>
-			</section>
-		</>
-	);
+            <div className="contactLink">
+              <a href="https://github.com/Mickyj70">GITHUB</a>
+              <a href="https://x.com/IAmMicky7?t=jc1z6hLxPSZk5wnRwFxa5w&s=09">
+                TWITTER
+              </a>
+              <a href="https://www.linkedin.com/in/michael-joseph-5458b5234?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+                LINKEDIN
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,54 +4,54 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 
 const Home = () => {
-	return (
-		<div className="home" id="home">
-			<div className="home-wrapper">
-				<motion.div
-					className="large-text"
-					initial={{ y: 20 }}
-					animate={{ y: 0 }}
-					transition={{ duration: 0.7 }}
-				>
-					<p>Hey, I&apos;m Michael </p>
-					<span>FullStack Developer</span>
-				</motion.div>
-				<motion.div
-					className="home-middle"
-					initial={{ y: 70 }}
-					animate={{ y: 0 }}
-					transition={{ duration: 0.7 }}
-				>
-					<button className="home-button">
-						<a href="#">
-							<FaGithub /> Github
-						</a>
-					</button>
-					<button className="home-button">
-						<a href="#">
-							<FaLinkedinIn /> LinkedIn
-						</a>
-					</button>
-					<button className="home-button">
-						<a href="#">
-							<HiOutlineMail /> Email
-						</a>
-					</button>
-				</motion.div>
-				<div className="small-text">
-					<motion.p
-						initial={{ y: 70, opacity: 0 }}
-						animate={{ y: 0, opacity: 1 }}
-						transition={{ duration: 0.4 }}
-					>
-						Hi, I&apos;m Michael an Experienced full-stack developer with over 2
-						years of experience in React, Next, Node js, and express js. Ready
-						to create innovative web solutions.
-					</motion.p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="home" id="home">
+      <div className="home-wrapper">
+        <motion.div
+          className="large-text"
+          initial={{ y: 20 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.7 }}
+        >
+          <p>Hey, I&apos;m Michael </p>
+          <span>FullStack Developer</span>
+        </motion.div>
+        <motion.div
+          className="home-middle"
+          initial={{ y: 70 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.7 }}
+        >
+          <button className="home-button">
+            <a href="https://github.com/Mickyj70">
+              <FaGithub /> Github
+            </a>
+          </button>
+          <button className="home-button">
+            <a href="https://www.linkedin.com/in/michael-joseph-5458b5234?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+              <FaLinkedinIn /> LinkedIn
+            </a>
+          </button>
+          <button className="home-button">
+            <a href="mailto:emrex1291@gmail.com">
+              <HiOutlineMail /> Email
+            </a>
+          </button>
+        </motion.div>
+        <div className="small-text">
+          <motion.p
+            initial={{ y: 70, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.4 }}
+          >
+            Hi, I&apos;m Michael an Experienced full-stack developer with over 2
+            years of experience in React, Next, Node js, and express js. Ready
+            to create innovative web solutions.
+          </motion.p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
